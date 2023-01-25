7df34375995f520335d72f3b3d8ebcd645250d48
Rahel T Teklu <rahelteklu0@gmail.com>
2023-01-24 22:43:50 -0600

    code added

--- RahelAirlinesApp/src/main/java/com/rahel/RahelAirlinesApp/Controller/FlightController.java
+++ RahelAirlinesApp/src/main/java/com/rahel/RahelAirlinesApp/Controller/FlightController.java
@@ -1,16 +1,21 @@
 package com.rahel.RahelAirlinesApp.Controller;
 
 import com.rahel.RahelAirlinesApp.Model.Flight;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@RestController
 public class FlightController {
+    @GetMapping("/search")
     public List<Flight> searchFlight(String departingCity, String destinationCity, LocalDate departingDate){
         return  null;
     }
-
+@GetMapping("/flight")
     public  Flight reserveFlight(){
         return  null;
     }
+
 }
--- RahelAirlinesApp/src/main/java/com/rahel/RahelAirlinesApp/Model/Person.java
+++ RahelAirlinesApp/src/main/java/com/rahel/RahelAirlinesApp/Model/Person.java
@@ -1,4 +1,47 @@
 package com.rahel.RahelAirlinesApp.Model;
 
 public class Person {
+    private String firstname;
+    private String lastname;
+    private String dateOfBirth;
+    private  Adress adress;
+
+    public Person(String firstname, String lastname, String dateOfBirth, Adress adress) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dateOfBirth = dateOfBirth;
+        this.adress = adress;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Adress getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
 }
