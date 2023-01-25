78d30dd1921b165f20e438dc400d35b06be8fa88
Rahel T Teklu <rahelteklu0@gmail.com>
2023-01-25 10:59:35 -0600

    controller code added

--- RahelAirlinesApp/src/main/java/com/rahel/RahelAirlinesApp/Controller/TravellerController.java
+++ RahelAirlinesApp/src/main/java/com/rahel/RahelAirlinesApp/Controller/TravellerController.java
@@ -1,4 +1,10 @@
 package com.rahel.RahelAirlinesApp.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TravellerController {
+    public boolean hasAnID(){
+        return false;
+    }
 }
--- RahelAirlinesApp/src/main/java/com/rahel/RahelAirlinesApp/Model/Traveller.java
+++ RahelAirlinesApp/src/main/java/com/rahel/RahelAirlinesApp/Model/Traveller.java
@@ -1,4 +1,41 @@
 package com.rahel.RahelAirlinesApp.Model;
 
-public class Traveller {
+public class Traveller extends Person {
+    private String creditCardInfo;
+    private String ID;
+    private  double Bags;
+    public Traveller(String firstname, String lastname, String dateOfBirth, Adress adress) {
+        super(firstname, lastname, dateOfBirth, adress);
+    }
+
+    public Traveller(String firstname, String lastname, String dateOfBirth, Adress adress, String creditCardInfo, String ID, double bags) {
+        super(firstname, lastname, dateOfBirth, adress);
+        this.creditCardInfo = creditCardInfo;
+        this.ID = ID;
+        Bags = bags;
+    }
+
+    public String getCreditCardInfo() {
+        return creditCardInfo;
+    }
+
+    public void setCreditCardInfo(String creditCardInfo) {
+        this.creditCardInfo = creditCardInfo;
+    }
+
+    public String ID() {
+        return ID;
+    }
+
+    public void ID(String ID) {
+        this.ID = ID;
+    }
+
+    public double getBags() {
+        return Bags;
+    }
+
+    public void setBags(double bags) {
+        Bags = bags;
+    }
 }
